e6e13ceffc2e99d4346709cd614e19e12b003661
Ivan Ermilov <iermilov@informatik.uni-leipzig.de>
2012-10-30 11:56:31 +0100

    saving wiki sparqlify config to the file

--- src/wikitoolsinterface.py
+++ src/wikitoolsinterface.py
@@ -10,8 +10,8 @@ class WikiToolsInterface:
         self.site.login(wikiconfig.username, password=wikiconfig.password)
         self.site.setMaxlag = -1 #will wait forever
         
-    def getPageContent(self, title):
-        params = {'action':'query', 'prop':'revisions', 'rvprop':'content', 'titles':title}
+    def getPageContent(self, entityName):
+        params = {'action':'query', 'prop':'revisions', 'rvprop':'content', 'titles':entityName}
         request = api.APIRequest(self.site, params)
         result = request.query()
         pages = result['query']['pages']
@@ -19,7 +19,36 @@ class WikiToolsInterface:
             page = pages[pageid]
             #get the last revision
             return page['revisions'][0]["*"]
+            
+    def getResourceConfiguration(self, entityName, resourceUrl):
+        pageContent = self.getPageContent(entityName)
+        resourceConfig = self._extractConfig(pageContent, resourceUrl)
+        filename = self._saveConfigToFile(entityName, resourceUrl, resourceConfig)
+        return filename
+    
+    def _extractConfig(self, pageContent, resourceUrl):
+        #strip nowiki tag
+        import re
+        output = re.sub(r'(<.*nowiki>)', r'', pageContent)
+        return output
+    
+    def _saveConfigToFile(self, entityName, resourceUrl, resourceConfig):
+        from database import Database
+        import os
+        newpath = 'sparqlify-mappings/'+entityName+'/'
+        db = Database(newpath)
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
+        #get entity
+        from ckaninterface import CkanInterface
+        ckan = CkanInterface()
+        resourceId = ckan.getResourceId(entityName, resourceUrl)
+        filename = resourceId + '.sparqlify'
+        db.saveDbaseRaw(filename, resourceConfig)
+        return filename
 
 if __name__ == '__main__':
     wt = WikiToolsInterface()
-    print wt.getPageContent('Data:abstraction-licences-in-force-and-new-licences-determined')
+    entityName = 'staff-organograms-and-pay-joint-nature-conservation-committee'
+    resourceUrl = 'http://data.defra.gov.uk/ops/jncc/Jncc+government-staff-and-salary-data-blank-template---March-2011-senior-data.csv'
+    print wt.getResourceConfiguration(entityName, resourceUrl)
