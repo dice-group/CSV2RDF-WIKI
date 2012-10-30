b9c9b3cef07ec2f167ddacc4d0bda885e160fe9c
Ivan Ermilov <iermilov@informatik.uni-leipzig.de>
2012-10-30 11:55:37 +0100

    templates partially moved to js side

--- src/pystachetempl.py
+++ src/pystachetempl.py
@@ -1,11 +1,8 @@
 import pystache
 
 class Index(object):
-    def __init__(self , entityName):
-        from ckaninterface import CkanInterface
-        ckan = CkanInterface()
-        self.entity = ckan.getEntity(entityName)
-        print self.entity
+    def __init__(self):
+        pass
         
 if __name__ == '__main__':
     entityName = "ambulance-call-outs-to-animal-attack-incidents"
