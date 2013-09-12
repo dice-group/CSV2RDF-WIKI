93df180553e8c54aeef9872f32c5ebe32e172ab6
Ivan Ermilov <akswadmin@csv2rdf.aksw.org>
2013-09-12 15:42:25 +0200

    small improvement on inject-template script

--- csv2rdf/scripts/inject-template.py
+++ csv2rdf/scripts/inject-template.py
@@ -4,9 +4,12 @@ import csv2rdf.tabular.mapping
 geo_qb_templates = "{{faceteCSV2RDF}}{{qbCSV2RDF}}"
 f = open("lessthanonemb.list", 'r')
 for line in f.readlines():
+    print id
     id = line.split('/')[-1].rstrip()
     mapping = csv2rdf.tabular.mapping.Mapping(resource_id = id)
     mapping.wiki_page = mapping.request_wiki_page()
+    if(not mapping.wiki_page):
+        continue
     (templates, template_start, template_end) = mapping.parse_template(mapping.wiki_page, 'CSV2RDFResourceLink')
     mapping.wiki_page = mapping.wiki_page.split('\n')
     split_place = int(template_end[0]) + 1
