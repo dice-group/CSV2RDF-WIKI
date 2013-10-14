cde85b680baf63e3ef1a063d96ede2562ea64d75
Ivan Ermilov <iermilov@informatik.uni-leipzig.de>
2013-10-14 15:41:14 +0200

    validation routine first test case added

--- csv2rdf/tabular/tabularfile.py
+++ csv2rdf/tabular/tabularfile.py
@@ -16,7 +16,7 @@ class TabularFile():
     def __init__(self, resource_id):
         self.id = resource_id
         self.filename = self.id
-    
+
     def download(self):
         resource = csv2rdf.ckan.resource.Resource(self.id)
         resource.init()
@@ -44,21 +44,21 @@ class TabularFile():
         db = csv2rdf.database.DatabasePlainFiles(csv2rdf.config.config.resources_path)
         db.delete(self.filename)
         return True
-        
+
     def get_csv_file_path(self):
         db = csv2rdf.database.DatabasePlainFiles(csv2rdf.config.config.resources_path)
         if(db.is_exists(self.filename)):
             return db.get_path_to_file(self.filename)
         else:
             return False
-        
+
     def get_csv_file_url(self):
         file_path = self.get_csv_file_path()
         if(file_path):
             return os.path.join(csv2rdf.config.config.server_base_url, file_path)
         else:
             return False
-    
+
     def read_resource_file(self):
         try:
             file = csv2rdf.database.DatabasePlainFiles(csv2rdf.config.config.resources_path)
@@ -66,13 +66,13 @@ class TabularFile():
         except BaseException as e:
             print "Could not read the resource! " + str(e)
             return False
-    
+
     def get_header_position(self):
         """
             Stub for the header recognition problem!
         """
         return 1
-    
+
     def extract_header(self, position):
         """
             This function take the first line of the csv file
@@ -88,7 +88,7 @@ class TabularFile():
             except BaseException as e:
                 print str(e)
                 return []
-    
+
     def validate(self):
         filename = self.filename
         (encoding, info) = self.get_info_about()
@@ -96,7 +96,7 @@ class TabularFile():
         logging.debug("File info: %s" % info)
 
         if(re.match('.*HTML.*', info) or
-           re.match('.*[Tt]orrent.*',info) or
+           re.match('.*[Tt]orrent.*', info) or
            self.isXML() or
            self.isHTML()):
             logging.debug("File is html, torrent or xml file ... Deleting")
@@ -112,11 +112,11 @@ class TabularFile():
             self._process_based_on(info, filename)
         else:
             return True
-    
+
     def get_info_about(self):
         """
             return (encoding, info) tuple
-            info is a plain string and has to be parsed 
+            info is a plain string and has to be parsed
         """
         db = csv2rdf.database.DatabasePlainFiles(csv2rdf.config.config.resources_path)
         filename = db.get_path_to_file(self.filename)
@@ -125,7 +125,7 @@ class TabularFile():
         encoding = mgc_encoding.from_file(filename)
         info = mgc_string.from_file(filename)
         return (encoding, info)
-    
+
     def isHTML(self):
         db = csv2rdf.database.DatabasePlainFiles(csv2rdf.config.config.resources_path)
         file_chunk = db.loadDbaseChunk(self.filename)
@@ -166,7 +166,7 @@ class TabularFile():
             logging.debug("Could not determine format ... Deleting.")
             self.delete()
             return False
-            
+
     def _process_xls(self, resource_id):
         print resource_id
         ssconvert_call = ["ssconvert", #from gnumeric package
@@ -179,11 +179,11 @@ class TabularFile():
         pipe_message = pipe.stdout.read()
         print pipe_message
         self.validate()
-    
+
     def _process_archive(self, filename):
         #unzip archive
         #check number of files
-        sevenza_call = ["7za", 
+        sevenza_call = ["7za",
                           "l",
                           csv2rdf.config.config.resources_path + filename]
         pipe = subprocess.Popen(sevenza_call, stdout=subprocess.PIPE)
@@ -197,7 +197,7 @@ class TabularFile():
             original_filename = re.search(pattern, pipe_message)
             original_filename = original_filename.group(0).split()[-1]
             #extract
-            sevenza_call = ["7za", 
+            sevenza_call = ["7za",
                             "e",
                             csv2rdf.config.config.resources_path + filename]
             pipe = subprocess.Popen(sevenza_call, stdout=subprocess.PIPE)
@@ -212,26 +212,26 @@ class TabularFile():
             #more than 1 file in the archive
             self.delete()
             return False
-    
+
     def _process_utf16(self, filename):
         f_in = open(filename, 'rU')
         f_out = open(filename+"-converted", 'wb')
-        
+
         for piece in self._read_in_chunks(f_in):
             converted_piece = piece.decode('utf-16-le', errors='ignore')
             converted_piece = converted_piece.encode('ascii', errors='ignore')
             f_out.write(converted_piece)
-        
+
         f_in.close()
         f_out.close()
-        
+
         #move converted to original
         mv_call = ["mv",
                     filename+"-converted",
                     filename]
         pipe = subprocess.Popen(mv_call, stdout=subprocess.PIPE)
         pipe_message = pipe.stdout.read()
-        
+
     def _read_in_chunks(self, file_object, chunk_size=1024):
         """Lazy function (generator) to read a file piece by piece.
         Default chunk size: 1k."""
@@ -247,10 +247,16 @@ class TabularFile():
         """
         csv_list = os.listdir(csv2rdf.config.config.resources_path)
         return csv_list
-            
+
 if __name__ == '__main__':
-    tabular_file = TabularFile('1aa9c015-3c65-4385-8d34-60ca0a875728')
-    print tabular_file.get_csv_file_url()
+
+    #Case 1: good CSV file
+    tabular_file = TabularFile('2daa0e60-4c36-487d-bb29-b3eba4e5ff0e')
+    tabular_file.download()
+    print tabular_file.validate()
+
+    #tabular_file = TabularFile('1aa9c015-3c65-4385-8d34-60ca0a875728')
+    #print tabular_file.get_csv_file_url()
     #print tabular_file.download()
     #print tabular_file.delete()
     #print tabular_file.get_csv_file_path()
