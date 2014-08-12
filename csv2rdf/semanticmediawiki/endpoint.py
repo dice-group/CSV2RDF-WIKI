fd8a50eb4f1e552646853039d782cbae44408e15
Ivan Ermilov <earthquakesan@gmail.com>
2014-08-12 12:13:49 +0200

    get resources from the same dataset - semanticmediawiki

--- csv2rdf/semanticmediawiki/endpoint.py
+++ csv2rdf/semanticmediawiki/endpoint.py
@@ -6,10 +6,33 @@ import requests
 import RDF
 
 class SMWEndpoint(object):
-    def __init__(self):
-        pass
+    propertyDatasetId = "http://wiki.publicdata.eu/wiki/Special:URIResolver/Property-3ADatasetId"
+    propertyDatasetUrl = "http://wiki.publicdata.eu/wiki/Special:URIResolver/Property-3ADatasetUrl"
+    propertyDatasetLabel = "http://wiki.publicdata.eu/wiki/Special:URIResolver/Property-3ADatasetLabel"
+    propertyResourceId = "http://wiki.publicdata.eu/wiki/Special:URIResolver/Property-3AResourceId"
+    propertyResourceLabel = "http://wiki.publicdata.eu/wiki/Special:URIResolver/Property-3AResourceLabel" 
+    propertyResourceUrl = "http://wiki.publicdata.eu/wiki/Special:URIResolver/Property-3AResourceUrl"
+    propertyCsvFileSize = "http://wiki.publicdata.eu/wiki/Special:URIResolver/Property-3ACsv_file_size"
+    propertyCsvNumberOfColumns = "http://wiki.publicdata.eu/wiki/Special:URIResolver/Property-3ACsv_number_of_columns"
+    propertyHasProperty = "http://wiki.publicdata.eu/wiki/Special:URIResolver/Property-3AHas_property"
+    propertyRdfLastSparqlifiedDate = "http://wiki.publicdata.eu/wiki/Special:URIResolver/Property-3ARdf_last_sparqlified"
+    propertyRdfLastSparqlified = "http://wiki.publicdata.eu/wiki/Special:URIResolver/Property-3ARdf_last_sparqlified-23aux"
+    propertyModificationDate = "http://wiki.publicdata.eu/wiki/Special:URIResolver/Property-3AModification_date-23aux"
+    propertyWikiPageModificationDate = "http://semantic-mediawiki.org/swivt/1.0#wikiPageModificationDate"
+    propertyPage = "http://semantic-mediawiki.org/swivt/1.0#page"
+    propertyLabel = "http://www.w3.org/2000/01/rdf-schema#label"
+    propertyWikiNamespace = "http://semantic-mediawiki.org/swivt/1.0#wikiNamespace"
 
-    def query(self, query):
+    def __init__(self):
+        self.model = RDF.Model()
+        self.parser = RDF.NTriplesParser()
+
+    def execute(self, query):
+        ntriplesString = self._sendPostToVirtuoso(query)
+        self.parser.parse_string_into_model(self.model, ntriplesString, 'http://localhost/')
+        return self.model
+
+    def _sendPostToVirtuoso(self, query):
         params = {
                     'default-graph-uri': '',
                     'query': query,
@@ -22,16 +45,55 @@ class SMWEndpoint(object):
         r = requests.post(wiki_sparql_url, data=params)
         return r.text
 
+
+    def _genResourceQuery(self, resourceUri):
+        return "CONSTRUCT {%s ?p ?o .} WHERE { %s ?p ?o.}" % (resourceUri, resourceUri, )
+
+    def _genPropertyQuery(self, propertyUri):
+        return "CONSTRUCT {?s ?p ?o .} WHERE { ?s ?p ?o . ?s <%s> %s}" % (self.propertyHasProperty, propertyUri,)
+
+    def _genDatasetQuery(self, datasetUri):
+        return "CONSTRUCT {?s ?p ?o .} WHERE { ?s ?p ?o . ?s <%s> <%s>}" % (self.propertyDatasetId, datasetUri,)
+
     def fetchResource(self, resourceId):
         resourceUri = self._genResourceUri(resourceId)
-        queryString = "CONSTRUCT {%s ?p ?o .} WHERE { %s ?p ?o.}" % (resourceUri, resourceUri, )
+        queryString = self._genResourceQuery(resourceUri)
+        print queryString
+        resourceMetaString = self.execute(queryString)
+        return self.model
+
+    def findSimilarResources(self, resourceId):
+        resourceModel = self.fetchResource(resourceId)
+        resourcePropertiesQuery = RDF.Query("SELECT ?o WHERE {?s <http://wiki.publicdata.eu/wiki/Special:URIResolver/Property-3AHas_property> ?o.}")
+        resourceProperties = resourcePropertiesQuery.execute(resourceModel)
+        for resourceProperty in resourceProperties:
+            prop = "<%s>" %(resourceProperty['o'],)
+            query = self._genPropertyQuery(prop)
+            print query
+            res = self.execute(query)
+            import ipdb; ipdb.set_trace()
+
+    def findResourceInSameDataset(self, resourceId):
+        """
+            Returns other resources in the dataset.
+            We assume that all resources in the same dataset have the same structure.
+        """
+        similarResources = []
+
+        resourceModel = self.fetchResource(resourceId)
+        resourceDatasetQuery = RDF.Query("SELECT ?o WHERE {?s <%s> ?o.}"%(self.propertyDatasetId,))
+        resourceDataset = str(resourceDatasetQuery.execute(resourceModel).next()['o'])
+        similarResourcesQuery = self._genDatasetQuery(resourceDataset)
         model = RDF.Model()
         parser = RDF.NTriplesParser()
-        resourceMetaString = self.query(queryString)
-        parser.parse_string_into_model(model, resourceMetaString, 'http://localhost/')
-        ser = RDF.NTriplesSerializer()
-        modelString = ser.serialize_model_to_string(model)
-        import ipdb; ipdb.set_trace()
+        ntriplesString = self._sendPostToVirtuoso(similarResourcesQuery)
+        parser.parse_string_into_model(model, ntriplesString, 'http://localhost/')
+        distinctResourceUris = RDF.Query("SELECT DISTINCT ?s WHERE {?s ?p ?o.}")
+        similarResourcesResult = distinctResourceUris.execute(model)
+        for resource in similarResourcesResult:
+            similarResources.append(str(resource['s']))
+        
+        return similarResources
 
     def _convertResourceIdToSparql(self, resourceId):
         resourceId = resourceId.split("-")
@@ -48,4 +110,4 @@ class SMWEndpoint(object):
 if __name__ == "__main__":
     resourceId = "02f31d80-40cc-496d-ad79-2cf02daa5675"
     endpoint = SMWEndpoint()
-    print endpoint.fetchResource(resourceId)
+    print endpoint.findResourceInSameDataset(resourceId)
